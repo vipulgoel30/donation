add1eea476cd7c7b838f557d96bf46ffcbecf85a
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-04-19 16:02:12 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,25 +43,26 @@ const Footer = () => {
 
                     <form onSubmit={(event) => {
                         event.preventDefault();
+                        subscribe()
                     }} className="w-full flex gap-4 flex-col">
                         <input className='w-full border text-sm px-3 py-2   bg-gray-200 rounded-lg hover:bg-white' type="email" name="" id="" placeholder='Email Address' required />
-                        <button onClick={subscribe} className="w-full font-semibold  px-3 py-2  bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg ">{sub}</button>
+                        <button className="w-full font-semibold  px-3 py-2  bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg ">{sub}</button>
                     </form>
 
 
                     <h1 className='font-bold text-xl'>CONNECT</h1>
                     <div className='flex gap-3 '>
                         <a href="https://www.instagram.com/">
-                        <AiFillInstagram className="text-4xl text-pink-600 cursor-pointer"/>
+                            <AiFillInstagram className="text-4xl text-pink-600 cursor-pointer" />
                         </a>
                         <a href="https://www.instagram.com/">
-                        <AiOutlineTwitter className="text-4xl text-gradient1b cursor-pointer" />
+                            <AiOutlineTwitter className="text-4xl text-gradient1b cursor-pointer" />
                         </a>
                         <a href="https://twitter.com/">
-                        <FaYoutube className="text-4xl text-red-600 cursor-pointer" />
+                            <FaYoutube className="text-4xl text-red-600 cursor-pointer" />
                         </a>
                         <a href="https://in.linkedin.com/">
-                            <FaLinkedinIn className="text-4xl text-blue-600 cursor-pointer"/>
+                            <FaLinkedinIn className="text-4xl text-blue-600 cursor-pointer" />
                         </a>
                     </div>
                 </div>
