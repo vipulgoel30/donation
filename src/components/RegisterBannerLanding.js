5cea56a8aabf2f81a3b19efe264a1d0fa4958feb
Vipul Goel <96120213+vipulgoel30@users.noreply.github.com>
2023-10-05 23:23:46 +0530

    Update RegisterBannerLanding.js

--- src/components/RegisterBannerLanding.js
+++ src/components/RegisterBannerLanding.js
@@ -24,7 +24,7 @@ export default function RegisterBannerLanding() {
                         Register as Donor
                     </span>
                 </Link>
-                <Link
+                <Link to ="/ngo/signup"
                     ><span
                         className="font-semibold inline-block bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg px-4 py-2 m-2"
                     >
